51e9fe4f769cf5afb5c3b97158ec59f43a7d810f
extrordinaire <maximoverzini@gmail.com>
2025-06-23 13:41:33 +0000

    Add all files

---
+++ astro.config.mjs
@@ -0,0 +1,50 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Psicopedagoga360",
+      logo: {
+        src: "./public/logo.png",
+      },
+      social: [
+        {
+          icon: "instagram",
+          label: "Instagram",
+          href: "https://www.instagram.com/psicopedagogamarianasavid/",
+        },
+        {
+          icon: "linkedin",
+          label: "LinkedIn",
+          href: "https://www.linkedin.com/in/mariana-savid-saravia-a11b17210/",
+        },
+        {
+          icon: "email",
+          label: "e-Mail",
+          href: "mailto:mariansavid@gmail.com",
+        },
+      ],
+      customCss: [
+        // Path to your Tailwind base styles:
+        "./src/styles/global.css",
+      ],
+      defaultLocale: "es",
+      locales: {
+        en: {
+          label: "English",
+        },
+        es: {
+          label: "Español",
+        },
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
